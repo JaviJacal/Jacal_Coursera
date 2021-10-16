33663e3565f22bc2f1ade7f8559aac69a990d506
Javier Jacal <javo10mil@gmail.com>
2021-10-16 14:41:40 -0500

    Adding coded functions

--- stats.c
+++ stats.c
@@ -37,35 +37,73 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-  //print_statistics(test,sizeof(test));
+  printf("\nThe original array is:\n");
+  print_array(test,sizeof(test));
+  sort_array(test,sizeof(test));
+  printf("\nThe sorted array is:\n");
+  print_array(test,sizeof(test));
+  print_statistics(test,sizeof(test));
 }
 
 /* Add other Implementation File Code Here */
 
 void print_statistics(unsigned char *data,unsigned int length) /*- A function that prints the statistics of an array including minimum, maximum, mean, and median.*/
 {
+   printf("\nThe maximum is: %u ",find_maximum(data,length));
+   printf("\nThe minimum is: %u ",find_minimum(data,length));
+   printf("\nThe mean is: %u ",find_mean(data,length));
+   printf("\nThe median is: %u ",find_median(data,length));
 }
 void print_array(unsigned char *data,unsigned int length) /*-  Given an array of data and a length, prints the array to the screen*/
 {
-
+   int i = 0;
+   for(i = 0; i<length; i++)
+   {
+      printf("%u ",data[i]);
+   }
 }
 unsigned char find_median(unsigned char *data,unsigned int length)/* - Given an array of data and a length, returns the median value*/
 {
-     return 0;
+   sort_array(data,length);
+   if(length%2==0)
+      return((data[length/2]+data[(length-1)/2])/2);
+
+   else
+   return(data[length/2]);
 }
 unsigned char find_mean(unsigned char *data,unsigned int length) /*-  Given an array of data and a length, returns the mean*/
 {
-     return 0;
+   int i = 0, sum = 0;
+   for(i = 0; i<length; i++)
+   {
+      sum += data[i]; 
+   }
+   return(sum/length);
 }
 unsigned char find_maximum(unsigned char *data,unsigned int length)/* -  Given an array of data and a length, returns the maximum*/
 {
-     return 0;
+   sort_array(data,length);
+   return(data[0]);
 }
 unsigned char find_minimum(unsigned char *data,unsigned int length) /*-  Given an array of data and a length, returns the minimum*/
 {
-   return 0;
+   sort_array(data,length);
+   return(data[length-1]);
 }
 void sort_array(unsigned char *data,unsigned int length)/*- Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last data(n-1) should be the smallest value. )*/
 {
-
+   int i,j;
+   unsigned char swap;
+   for (i = 0 ; i < length - 1; i++)
+  {
+    for (j = 0 ; j < length - i - 1; j++)
+    {
+      if (data[j] < data[j+1]) 
+      {
+        swap       = data[j];
+        data[j]   = data[j+1];
+        data[j+1] = swap;
+      }
+    }
+  }
 }
